3ad532dcb7e7ba981c8f94b41358ccd51c424559
Jisang Moon <ans939@naver.com>
2022-03-31 17:46:17 +0900

    second commit

--- 12.이미지변형(원근).py
+++ 12.이미지변형(원근).py
@@ -8,7 +8,7 @@ Created on Thu Mar 31 16:36:28 2022
 import cv2
 import numpy as np
 
-img = cv2.imread('paper.jpg')
+'''img = cv2.imread('paper.jpg')
 
 width, height = 640, 240 #가로크기 640, 세로크기 240으로 결과물 출력
 
@@ -22,11 +22,11 @@ result = cv2.warpPerspective(img, matrix, (width, height)) #matrix 대로 변환
 cv2.imshow('img', img)
 cv2.imshow('result', result)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()'''
 
 
 
-'''img = cv2.imread('poker.jpg')
+img = cv2.imread('poker.jpg')
 
 width, height = 530, 710
 
@@ -40,4 +40,4 @@ result = cv2.warpPerspective(img, matrix, (width, height)) #matrix 대로 변환
 cv2.imshow('img', img)
 cv2.imshow('result', result)
 cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+cv2.destroyAllWindows()
